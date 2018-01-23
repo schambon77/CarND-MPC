006146b26cb9b1f21768dc5f4f2d182c4df6ac07
schambon77 <30735585+schambon77@users.noreply.github.com>
2018-01-22 21:47:29 -0600

    First pass

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -134,12 +134,12 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   size_t i;
   typedef CPPAD_TESTVECTOR(double) Dvector;
 
-  double x = x0[0];
-  double y = x0[1];
-  double psi = x0[2];
-  double v = x0[3];
-  double cte = x0[4];
-  double epsi = x0[5];
+  double x = state[0];
+  double y = state[1];
+  double psi = state[2];
+  double v = state[3];
+  double cte = state[4];
+  double epsi = state[5];
 
   // Number of model variables (includes both states and inputs).
   size_t n_vars = N * 6 + (N - 1) * 2;

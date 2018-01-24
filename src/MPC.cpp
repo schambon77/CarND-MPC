04b2de816532c763cb5d01caafedf7a8fe50d47a
schambon77 <30735585+schambon77@users.noreply.github.com>
2018-01-23 21:47:01 -0600

    add debug traces

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -105,16 +105,20 @@ class FG_eval {
 	  AD<double> delta0 = vars[delta_start + t - 1];
 	  AD<double> a0 = vars[a_start + t - 1];
 
+      std::cout << "Before computing f0" << std::endl;
+
 	  AD<double> f0 = 0.0;
 	  for (t = 0; t < coeffs.size(); t++) {
 	    f0 += coeffs[t] * CppAD::pow(x0, t);
 	  }
+      std::cout << "f0: " << f0 << std::endl;
 
 	  AD<double> psides0 = 0.0;
 	  for (t = 1; t < coeffs.size(); t++) {
 	    psides0 += coeffs[t] * t * CppAD::pow(x0, t - 1);
 	  }
 	  psides0 = CppAD::atan(psides0);
+      std::cout << "psides0: " << psides0 << std::endl;
 
 	  // Equations for the model:
 	  // x_[t+1] = x[t] + v[t] * cos(psi[t]) * dt

37f53e22a666434af213ed8db2084f5eec6c10c2
schambon77 <30735585+schambon77@users.noreply.github.com>
2018-01-25 21:19:45 -0600

    Add mpc points

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -6,7 +6,7 @@
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 50;
+size_t N = 500;
 double dt = 0.1;
 
 // This value assumes the model presented in the classroom is used.

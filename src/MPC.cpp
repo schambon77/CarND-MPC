4d2b04f8f30f9fb0019853b4407b8c3826096c2b
schambon77 <30735585+schambon77@users.noreply.github.com>
2018-01-29 21:35:23 -0600

    change cost

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -6,7 +6,7 @@
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 30;
+size_t N = 50;
 double dt = 0.05;
 
 // This value assumes the model presented in the classroom is used.
@@ -51,7 +51,7 @@ class FG_eval {
 
 	// The part of the cost based on the reference state.
 	for (t = 0; t < N; t++) {
-	  fg[0] += CppAD::pow(vars[cte_start + t], 2);
+	  fg[0] += 100*CppAD::pow(vars[cte_start + t], 2);
 	  fg[0] += CppAD::pow(vars[epsi_start + t], 2);
 	  fg[0] += 100*CppAD::pow(vars[v_start + t] - ref_v, 2);
 	}
@@ -123,7 +123,7 @@ class FG_eval {
 	  // epsi[t+1] = psi[t] - psides[t] + v[t] * delta[t] / Lf * dt
 	  fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
 	  fg[1 + y_start + t] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
-	  fg[1 + psi_start + t] = psi1 - (psi0 + v0 * (delta0 / Lf) * dt);  //model equation modified to factor positive angle means right turn
+	  fg[1 + psi_start + t] = psi1 - (psi0 - v0 * (delta0 / Lf) * dt);  //model equation modified to factor positive angle means right turn
 	  fg[1 + v_start + t] = v1 - (v0 + a0 * dt);
 	  fg[1 + cte_start + t] = cte1 - ((f0 - y0) + (v0 * CppAD::sin(epsi0) * dt));
 	  fg[1 + epsi_start + t] = epsi1 - ((psi0 - psides0) + v0 * (delta0 / Lf) * dt);

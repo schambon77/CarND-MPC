134fd32c36706ecf60963dc25c066aa73276096a
schambon77 <30735585+schambon77@users.noreply.github.com>
2018-01-30 21:01:32 -0600

    change cost

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -21,7 +21,7 @@ double dt = 0.05;
 // This is the length from front to CoG that has a similar radius.
 const double Lf = 2.67;
 
-double ref_v = 50;
+double ref_v = 10;
 
 size_t x_start = 0;
 size_t y_start = x_start + N;
@@ -51,7 +51,7 @@ class FG_eval {
 
 	// The part of the cost based on the reference state.
 	for (t = 0; t < N; t++) {
-	  fg[0] += 60*CppAD::pow(vars[cte_start + t], 2);
+	  fg[0] += CppAD::pow(vars[cte_start + t], 2);
 	  fg[0] += CppAD::pow(vars[epsi_start + t], 2);
 	  fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);
 	}
